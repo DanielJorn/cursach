f93abd9b7054c7cba2a6e85a975faad759b456a2
aproshchenko <aproshchenko.appwell@ancestry.com>
2022-11-17 20:21:25 +0200

    crossfade / dummies

--- app/src/main/java/com/example/cursach/screens/articles/ArticlesScreen.kt
+++ app/src/main/java/com/example/cursach/screens/articles/ArticlesScreen.kt
@@ -1,18 +1,20 @@
 package com.example.cursach.screens.articles
 
+import androidx.compose.animation.Crossfade
+import androidx.compose.animation.core.*
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment.Companion.CenterEnd
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -55,36 +57,45 @@ fun NewsFeed(
             .padding(padding)
             .background(MaterialTheme.colors.background)
     ) {
-        LazyColumn {
-            items(state.articles) {
-                Card(Modifier.padding(6.dp)) {
-                    Row(
-                        Modifier
-                            .padding(16.dp)
-                            .fillMaxWidth()
-                    ) {
-                        Column {
-                            Text(
-                                text = it.author,
-                                style = MaterialTheme.typography.caption,
-                                color = Color.Gray
-                            )
-                            Text(
-                                text = it.title,
-                                style = MaterialTheme.typography.h5,
-                            )
-                            Spacer(Modifier.size(4.dp))
-                            Text(it.subtitle, style = MaterialTheme.typography.subtitle1)
-                        }
-                        Box(Modifier.fillMaxSize()) {
-                            Image(
-                                modifier = Modifier
-                                    .size(50.dp)
-                                    .clip(MaterialTheme.shapes.medium)
-                                    .align(CenterEnd),
-                                painter = painterResource(id = R.drawable.ic_launcher_background),
-                                contentDescription = "",
-                            )
+        Crossfade(targetState = state, animationSpec = tween(900)) {
+            when {
+                it.articles.isEmpty() -> LazyColumn(userScrollEnabled = false) {
+                    items(10) {
+                        Dummy()
+                    }
+                }
+                else -> LazyColumn {
+                    items(it.articles) {
+                        Card(Modifier.padding(6.dp)) {
+                            Row(
+                                Modifier
+                                    .padding(16.dp)
+                                    .fillMaxWidth()
+                            ) {
+                                Column {
+                                    Text(
+                                        text = it.author,
+                                        style = MaterialTheme.typography.caption,
+                                        color = Color.Gray
+                                    )
+                                    Text(
+                                        text = it.title,
+                                        style = MaterialTheme.typography.h5,
+                                    )
+                                    Spacer(Modifier.size(4.dp))
+                                    Text(it.subtitle, style = MaterialTheme.typography.subtitle1)
+                                }
+                                Box(Modifier.fillMaxSize()) {
+                                    Image(
+                                        modifier = Modifier
+                                            .size(50.dp)
+                                            .clip(MaterialTheme.shapes.medium)
+                                            .align(CenterEnd),
+                                        painter = painterResource(id = R.drawable.ic_launcher_background),
+                                        contentDescription = "",
+                                    )
+                                }
+                            }
                         }
                     }
                 }
@@ -92,3 +103,45 @@ fun NewsFeed(
         }
     }
 }
+
+
+@Composable
+private fun Dummy() {
+    val transition = rememberInfiniteTransition()
+    val rightGradientBorder by transition.animateFloat(
+        initialValue = 0f,
+        targetValue = 1f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(
+                durationMillis = 2000,
+                delayMillis = listOf(100, 200, 300).random(),
+            ),
+            repeatMode = RepeatMode.Reverse
+        )
+    )
+    val a = (50..80).random() / 100f
+    Card(
+        modifier = Modifier
+            .alpha(0.7f)
+            .padding(6.dp)
+    ) {
+        Box(
+            Modifier
+                .fillMaxWidth(a)
+                .height(100.dp)
+                .drawBehind {
+                    drawRect(
+                        brush = Brush.horizontalGradient(
+                            0f to Color.Gray,
+                            rightGradientBorder to Color.White,
+                            1f to Color.Gray,
+                        ),
+                        alpha = 0.5f
+                    )
+                }
+        ) {
+
+        }
+    }
+}
+
